01fb79525a854fb6036c3e032edd0acc5a73732e
sakshihase100 <sakshihase100@gmail.com>
2025-09-09 20:24:01 +0530

    Create day26.java

---
+++ day26.java
@@ -0,0 +1,60 @@
+import java.util.*;
+
+public class CycleInUndirectedGraph {
+    public static boolean hasCycle(int V, List<List<Integer>> adj) {
+        boolean[] visited = new boolean[V];
+
+        for (int i = 0; i < V; i++) {
+            if (!visited[i]) {
+                if (dfs(i, -1, visited, adj)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private static boolean dfs(int node, int parent, boolean[] visited, List<List<Integer>> adj) {
+        visited[node] = true;
+
+        for (int neighbor : adj.get(node)) {
+            if (!visited[neighbor]) {
+                if (dfs(neighbor, node, visited, adj)) {
+                    return true;
+                }
+            } 
+            else if (neighbor != parent) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of vertices: ");
+        int V = sc.nextInt();
+
+        System.out.print("Enter number of edges: ");
+        int E = sc.nextInt();
+
+        List<List<Integer>> adj = new ArrayList<>();
+        for (int i = 0; i < V; i++) {
+            adj.add(new ArrayList<>());
+        }
+
+        System.out.println("Enter edges (u v):");
+        for (int i = 0; i < E; i++) {
+            int u = sc.nextInt();
+            int v = sc.nextInt();
+            adj.get(u).add(v);
+            adj.get(v).add(u); 
+        }
+
+        boolean result = hasCycle(V, adj);
+        System.out.println("Cycle present? " + result);
+
+        sc.close();
+    }
+}
